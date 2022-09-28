ada04f2465bd81c4235fe7a6e1c251b0cefb9a80
dlxx <dlxxdlcc@gmail.com>
2022-09-29 02:35:49 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import axios from 'axios'
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    clickButton() {
+      axios.get('/test').then((e) => {console.log(e)}).catch((err) => {})
+    }
+
+  }
+}
+</script>
+
+<template>
+  <button @click="clickButton" >click</button>
+</template>
+
+<style scoped>
+
+</style>
